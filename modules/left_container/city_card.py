68095acbbe99ebc0944af81655567b3e33ed805b
unknown <mr.olegyou@gmail.com>
2024-12-09 20:56:07 +0200

    json test data added

--- modules/left_container/city_card.py
+++ modules/left_container/city_card.py
@@ -1,10 +1,12 @@
 import customtkinter as ctk
 from .scroll_frame import vertical_scroll
 import requests
+import json
 
 import datetime #библиотека для работы со временем (Timezone("Europe/Budapest") => "06.12.2024Z19:49:15")
 import zoneinfo #конвертация текста в таймзону ("Europe/Budapest" => Timezone("Europe/Budapest"))
 from timezonefinder import TimezoneFinder #конвертация координат в название зоны (51.254;26.678 => "Europe/Budapest")
+
 
 class City_Frame(ctk.CTkFrame):
     def __init__(self, child_master, city_name, time, temp, condition, min_max):
@@ -64,24 +66,39 @@ class City_Frame(ctk.CTkFrame):
 
 cities = ["Dnipro", "Kyiv", "Budapest", "Warsaw", "Vienna", "Prague", "Berlin", "Milan", "Paris", "London", "NewYork"] 
 
+my_city_info = requests.get("https://ipinfo.io/json")
+my_city_name = my_city_info.json()["city"]
+
+if my_city_name not in cities: #не в (списке, словаре)
+    cities.append(my_city_name) 
+
 for city_name in cities:
 
     url = f"https://wttr.in/{city_name}?format=j1"
     result = requests.get(url)
 
+    with open("data_base.json", "r") as file:
+        added_list = json.load(file)
+        if len(cities) > len(added_list):
+            added_list.append(result.json())
+
+    with open("data_base.json", "w") as file:
+        json.dump(added_list, file)
+    
     # time = result.json()["current_condition"][0]["localObsDateTime"].split(" ")[1]
     # temp = result.json()["current_condition"][0]["temp_C"] + "°"
     # condition = result.json()["current_condition"][0]["weatherDesc"][0]["value"]
     # min_max = result.json()["weather"][0]["mintempC"] + " " + result.json()["weather"][0]["maxtempC"]
     
-    my_lat = result.json()["nearest_area"][0]["latitude"]
-    my_long = result.json()["nearest_area"][0]["longitude"]
+    # my_lat = result.json()["nearest_area"][0]["latitude"]
+    # my_long = result.json()["nearest_area"][0]["longitude"]
     
-    timezona_name = TimezoneFinder().timezone_at(lat = float(my_lat), lng = float(my_long))
-    zona = zoneinfo.ZoneInfo(timezona_name)
-    my_time = datetime.datetime.now(zona)
+    # timezona_name = TimezoneFinder().timezone_at(lat = float(my_lat), lng = float(my_long))
+    # zona = zoneinfo.ZoneInfo(timezona_name)
+    # my_time = datetime.datetime.now(zona)
 
-    time = my_time.strftime("%H:%M")
+    # time = my_time.strftime("%H:%M")
+    time = "15:15"
     temp = "20°"
     condition = "sunny"
     min_max = "мин. 15, макс. 26"
